a8b6aa3d59a73543c98101f0a58395c5c2a56487
Gergo Buglyo <buglyogergo@gmail.com>
2023-05-23 23:26:05 +0200

    Created Main copmonent

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,40 @@
+import { useContext } from "react";
+import { GameContext } from "../../context/GameContext";
+import { v4 as uuidv4 } from "uuid";
+
 function Main() {
-  return <div>Main</div>;
+  const { categories, fetchAPI } = useContext(GameContext) || {};
+
+  const listOfCategories: string[] = categories ? Object.keys(categories) : [];
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLButtonElement;
+    const value = target.innerHTML.replace(/&amp;/g, "&");
+
+    //TODO: send a request to the API
+    if (fetchAPI) {
+      fetchAPI(value);
+    }
+  };
+
+  return (
+    <div className="flex flex-col  h-5/6">
+      <h1 className="text-5xl">Trivia Quiz</h1>
+      <div className="flex flex-col justify-center  h-full">
+        {listOfCategories.map((category) => {
+          return (
+            <button
+              key={uuidv4()}
+              className="text-gray-100 py-2 px-8 my-1 dark:bg-pink-500 bg-pink-700 duration-200 rounded-lg hover:dark:bg-pink-700 hover:bg-pink-500 font-bold"
+              onClick={handleClick}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Main;
--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -7,6 +7,12 @@ interface MyContextProviderProps {
 interface GameContextType {
   openWindow: boolean;
   setOpenWindow: React.Dispatch<React.SetStateAction<boolean>>;
+  categories: object;
+  fetchAPI: (category: string) => void;
+}
+
+interface Categories {
+  [key: string]: [];
 }
 
 const GameContext = createContext<GameContextType | null>(null);
@@ -15,10 +21,44 @@ const GameContextProvider: React.FC<MyContextProviderProps> = ({
   children,
 }) => {
   const [openWindow, setOpenWindow] = useState<boolean>(true);
+  const [categories, setCategories] = useState<Categories>({});
+  // save the played questions, so that they cannot be played again
+  const [playedQuestions, setPlayedQuestions] = useState<string[]>([]);
+
+  // get list of the categories
+  useState(async () => {
+    const response = await fetch("https://the-trivia-api.com/api/categories");
+    const data = await response.json();
+    setCategories(data);
+  });
+
+  // Fetch puzzle in the Main component
+  //TODO: figure out how to contain the played puzzles and check if the puzzle have been played yet
+  const fetchAPI = async (category: string): Promise<void> => {
+    const categoryQuery = categories[category];
+    const response = await fetch(
+      `https://the-trivia-api.com/api/questions?categories=${categoryQuery[0]}&type=`
+    );
+    const data = await response.json();
+
+    let random = Math.floor(Math.random() * data.length);
+    let puzzle = data[random];
+
+    if (playedQuestions.includes(puzzle.id)) {
+      random = Math.floor(Math.random() * data.length);
+      puzzle = data[random];
+    } else {
+      setPlayedQuestions((playedQuestions) => [...playedQuestions, puzzle.id]);
+    }
+
+    console.log(puzzle);
+  };
 
   const contextValue: GameContextType = {
     openWindow,
     setOpenWindow,
+    categories,
+    fetchAPI,
   };
 
   return (
